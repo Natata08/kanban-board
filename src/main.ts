41eb17140e47448fde2ae98a819bd40a402c5b21
Natalia Lapina <lapinann08@gmail.com>
2025-06-09 19:01:19 +0200

    feat: add colors for light and dark themes

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,36 @@ const vuetify = createVuetify({
   directives,
   theme: {
     defaultTheme: 'light',
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: '#1976D2',
+          secondary: '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+          background: '#F5F5F5',
+          surface: '#FFFFFF',
+        },
+      },
+      dark: {
+        dark: true,
+        colors: {
+          primary: '#90CAF9',
+          secondary: '#BDBDBD',
+          accent: '#B388FF',
+          error: '#FF8A80',
+          info: '#64B5F6',
+          success: '#81C784',
+          warning: '#FFB74D',
+          background: '#121212',
+          surface: '#1E1E1E',
+        },
+      },
+    },
   },
 })
 
